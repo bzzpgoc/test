e9845c1efe60bbffc692db1bac17d58a16542e72
BZZPGOC <BZZPGOC@CCSD-45HYBH2.ccsdut.net>
2019-01-14 10:02:36 -0700

    init commit

---
+++ test/src/pck/Class.java
@@ -0,0 +1,9 @@
+package pck;
+
+public class Class
+{
+	public static void main(String[] args)
+		{
+			System.out.println("true");
+		}
+}
---
+++ test/src/pck/Test.java
@@ -0,0 +1,10 @@
+package pck;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
